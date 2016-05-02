43a77d72d40482cc201d6fb43b5c06ebd54a525d
Guy <guy@burste.in>
2016-05-02 12:30:57 -0400

    THIS WORKS GREAT! FAKE PAGE!

--- app/src/main/java/cosc150/restaurantsearch/RestaurantSearch.java
+++ app/src/main/java/cosc150/restaurantsearch/RestaurantSearch.java
@@ -2,6 +2,10 @@ package cosc150.restaurantsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RestaurantSearch extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class RestaurantSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_search);
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
+        String[] values = new String[]{"Michaucan Restaurant", "Taco Cantina",
+                "Cinco Mayo", "Azteca Mex", "The Azteca", "Somato", "Grand Azteca",
+                "Plaza Azteca", "Aztec Cantina" , "El Pollo Loco", "Big Joses",
+                "Garcia and Martinez' Grill", "Margarita's Margitas",
+                "We're number Juan", "Jose's and Josb's" , "Grill and Steak"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
 }
