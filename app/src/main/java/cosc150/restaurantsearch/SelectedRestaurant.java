c7d3d968461aeac07633a1349ca89f182ec50dc2
Guy <guy@burste.in>
2016-05-08 17:16:56 -0400

    Added Maps API

--- app/src/main/java/cosc150/restaurantsearch/SelectedRestaurant.java
+++ app/src/main/java/cosc150/restaurantsearch/SelectedRestaurant.java
@@ -75,7 +75,8 @@ public class SelectedRestaurant extends AppCompatActivity {
     }
 
     public void goToMap (View view) {
-        goToUrl("https://www.google.com/maps/place/O'Donovan+Dining+Hall,+Washington,+DC+20007/@38.9063818,-77.0757277,19z/data=!3m1!4b1!4m6!1m3!3m2!1s0x89b7b640ce9f8f6b:0x1c9d6d0554662882!2sGeorgetown+University!3m1!1s0x89b7b64169d9932b:0xb576adcc47fbda61");
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapIntent);
     }
 
     protected void goToUrl (String url) {
