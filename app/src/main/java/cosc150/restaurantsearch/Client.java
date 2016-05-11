9e7db711eac4d51d70372af8462d33f2a112e616
matthewchiang <matthewchiang18@gmail.com>
2016-05-11 13:32:53 -0400

    Updated Client Manually w/ code to make it work with server

--- app/src/main/java/cosc150/restaurantsearch/Client.java
+++ app/src/main/java/cosc150/restaurantsearch/Client.java
@@ -18,13 +18,11 @@ public class Client {
     private String hostname = "52.90.92.72";
     private int portNumber = 40001;
     private Socket connection = null;
-    private ArrayList<String> toRequest;
-    private BPlusTree bPlusTreeToInsert;
+    private RestaurantSearch restaurantSearch;
 
-    Client(ArrayList<String> toRequest, BPlusTree bPlusTreeToInsert) throws IOException{
-        this.toRequest = toRequest;
-        this.bPlusTreeToInsert = bPlusTreeToInsert;
-        new messaging(this.hostname, this.portNumber, toRequest, bPlusTreeToInsert);
+    Client(RestaurantSearch restaurantSearch) throws IOException{
+        this.restaurantSearch = restaurantSearch;
+        new messaging(this.hostname, this.portNumber, restaurantSearch);
     }
 
     public void close_connection() throws IOException{
@@ -35,15 +33,13 @@ public class Client {
         String hostname;
         int portNumber;
         Socket connectedSocket = null;
-        ArrayList<String> requested;
-        BPlusTree bPlusTree;
+        RestaurantSearch restaurantSearch;
 
         // Thread constructor
-        messaging(String ip, int port, ArrayList<String> requested, BPlusTree bPlusTree){
+        messaging(String ip, int port, RestaurantSearch restaurantSearch){
             this.hostname = ip;
             this.portNumber = port;
-            this.requested = requested;
-            this.bPlusTree = bPlusTree;
+            this.restaurantSearch = restaurantSearch;
             start();
         }
 
@@ -65,13 +61,14 @@ public class Client {
 
                         // Check if file is incoming.
                         if(message.charAt(0) == '$'){
+                            System.out.println("Recieved message!");
                             // Parse message and begin recieving file.
                             message = message.substring(1);
                             int fileSize = Integer.parseInt(message.substring(0, message.indexOf('$')));
-                            String fileName = message.substring(message.lastIndexOf('$') + 1);
 
-                            int BLOCKSIZE = 1024;
-                            byte[] byteSize = new byte[BLOCKSIZE];
+                            System.out.println(fileSize);
+
+                            byte[] byteSize = new byte[fileSize];
                             // Byte stream connection from socket
                             InputStream incoming = connectedSocket.getInputStream();
                             // New file directory
@@ -79,37 +76,49 @@ public class Client {
                             // Stream to extract file from byte stream
                             BufferedOutputStream output = new BufferedOutputStream(outputData);
 
+                            int left = fileSize;
+
                             try{
-                                int i = BLOCKSIZE, j = 0;
-                                while (i < fileSize){
-                                    while (incoming.available() < BLOCKSIZE)
-                                        continue;
-                                    incoming.read(byteSize, 0, BLOCKSIZE);
-                                    output.write(byteSize, 0, BLOCKSIZE);
-                                    i += BLOCKSIZE;
-                                }
-                                i -= BLOCKSIZE;
-                                while (i + j < fileSize){
-                                    incoming.read(byteSize, 0, 1);
-                                    output.write(byteSize, 0, 1);
-                                    j++;
+                                long timeStart = System.currentTimeMillis();
+                                int i = 0, j = 0;
+                                while (i < fileSize) { // && System.currentTimeMillis() - timeStart < 10000){
+//                                    while (incoming.available() < BLOCKSIZE)
+//                                        continue;
+                                    int actualRead = incoming.read(byteSize, i, left);
+                                    output.write(byteSize, i, actualRead);
+                                    i += actualRead;
+                                    left -= actualRead;
+                                    System.out.println(i);
+                                    System.out.println(new String(byteSize));
                                 }
 
-                                String allData = outputData.toString();
+                                System.out.println("here");
+
+                                String allData = new String(byteSize);
                                 System.out.print(allData);
                                 Scanner scanner = new Scanner(allData);
-
+                                int failed = 0;
                                 while (scanner.hasNextLine()) {
-                                    String toInsert = scanner.nextLine();
-                                    String categoryToInsert = toInsert.substring(0, toInsert.indexOf('@'));
-                                    String nameToInsert = toInsert.substring(toInsert.indexOf('@') + 1, toInsert.lastIndexOf('@'));
-                                    String descriptionToInsert = toInsert.substring(toInsert.lastIndexOf('@'), toInsert.length() - 3);
-                                    descriptionToInsert = descriptionToInsert.trim();
-                                    double ratingToInsert = Double.valueOf(toInsert.substring(toInsert.length()-3));
-                                    bPlusTree.insert(new Restaurant(categoryToInsert, nameToInsert, descriptionToInsert, ratingToInsert));
+                                    try {
+                                        String toInsert = scanner.nextLine();
+                                        //System.out.print(toInsert);
+                                        String categoryToInsert = toInsert.substring(0, toInsert.indexOf('@'));
+                                        String nameToInsert = toInsert.substring(toInsert.indexOf('@') + 1, toInsert.lastIndexOf('@'));
+                                        String descriptionToInsert = toInsert.substring(toInsert.lastIndexOf('@') + 1, toInsert.length() - 3);
+                                        descriptionToInsert = descriptionToInsert.trim();
+                                        double ratingToInsert = Double.valueOf(toInsert.substring(toInsert.length() - 3));
+                                        restaurantSearch.allRestaurants.insert(new Restaurant(categoryToInsert, nameToInsert, descriptionToInsert, ratingToInsert));
+                                    } catch (NumberFormatException e){
+                                        failed++;
+                                        System.out.println("Failed input row: " + failed);
+                                    } catch (StringIndexOutOfBoundsException e){
+                                        failed++;
+                                        System.out.println("Failed input row: " + failed);
+                                    }
                                 }
+                                restaurantSearch.readyToDisplay = new Boolean(true);
                                 scanner.close();
-
+                                System.out.println("Exiting message receive!");
                             }
                             finally{
                                 output.flush();
@@ -122,10 +131,10 @@ public class Client {
                     // Check if message is ready to be sent.
                     String toSend = "";
                     boolean requestReady = false;
-                    while(requested.size() > 0){
+                    while(restaurantSearch.categoriesToSearch.size() > 0){
                         // Convert message to message protocol.
-                        toSend += requested.get(0) + '%';
-                        requested.remove(0);
+                        toSend += restaurantSearch.categoriesToSearch.get(0) + '%';
+                        restaurantSearch.categoriesToSearch.remove(0);
                         requestReady = true;
                     }
                     if (requestReady) {
