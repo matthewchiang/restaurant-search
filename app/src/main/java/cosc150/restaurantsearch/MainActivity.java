8dc5a965211d085e344dc0774f6bffb676f3a1b1
Nitzan Gabai <neg27@georgetown.edu>
2016-05-12 17:26:14 -0400

    Update MainActivity.java

--- app/src/main/java/cosc150/restaurantsearch/MainActivity.java
+++ app/src/main/java/cosc150/restaurantsearch/MainActivity.java
@@ -23,13 +23,14 @@ public class MainActivity extends AppCompatActivity {
     ImageView canadian;
     ImageView mexican;
 
-    boolean[] selected = new boolean[8];
+    boolean[] selected = new boolean[8]; //Boolean selection flags 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Loading icons on to the ImageViews
         aztec = (ImageView)findViewById(R.id.Aztec);
         chinese = (ImageView)findViewById(R.id.Chinese);
         japanese = (ImageView)findViewById(R.id.Japanese);
@@ -42,76 +43,77 @@ public class MainActivity extends AppCompatActivity {
         aztec.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[0] = !selected[0];
-                if (selected[0])
+                if (selected[0]) //if icon is pressed, turn its shade red to identify selection
                     aztec.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     aztec.clearColorFilter();
             }
         });
         chinese.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[1] = !selected[1];
-                if (selected[1])
+                if (selected[1]) //if icon is pressed, turn its shade red to identify selection
                     chinese.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     chinese.clearColorFilter();
             }
         });
         japanese.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[2] = !selected[2];
-                if (selected[2])
+                if (selected[2]) //if icon is pressed, turn its shade red to identify selection
                     japanese.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     japanese.clearColorFilter();
             }
         });
         italian.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[3] = !selected[3];
-                if (selected[3])
+                if (selected[3]) //if icon is pressed, turn its shade red to identify selection
                     italian.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     italian.clearColorFilter();
             }
         });
         french.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[4] = !selected[4];
-                if (selected[4])
+                if (selected[4]) //if icon is pressed, turn its shade red to identify selection
                     french.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     french.clearColorFilter();
             }
         });
         middleEastern.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[5] = !selected[5];
-                if (selected[5])
+                if (selected[5]) //if icon is pressed, turn its shade red to identify selection
                     middleEastern.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     middleEastern.clearColorFilter();
             }
         });
         canadian.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[6] = !selected[6];
-                if (selected[6])
+                if (selected[6]) //if icon is pressed, turn its shade red to identify selection
                     canadian.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     canadian.clearColorFilter();
             }
         });
         mexican.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selected[7] = !selected[7];
-                if (selected[7])
+                if (selected[7]) //if icon is pressed, turn its shade red to identify selection
                     mexican.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
-                else
+                else //if pressed again, turn off red shade
                     mexican.clearColorFilter();
             }
         });
 
+        //When 'search' button is pressed, call the intent function below
         final Button searchButton = (Button) findViewById(R.id.searchButton);
         searchButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -120,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Creates intent sent to next activity including the boolean selection flags of categories
     public void switchToRestaurantSearch() {
         Intent restaurantSearchIntent = new Intent(this, RestaurantSearch.class);
         restaurantSearchIntent.putExtra("selected", selected);
